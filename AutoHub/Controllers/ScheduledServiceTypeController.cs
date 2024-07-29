5a3839ec2953e0583d55b592cdc1d7500367013a
Dominic Fernandez <dominic.fernandez1023@gmail.com>
2024-07-28 18:17:55 -0700

    sst controller

--- AutoHub/Controllers/ScheduledServiceTypeController.cs
+++ AutoHub/Controllers/ScheduledServiceTypeController.cs
@@ -40,12 +40,42 @@ namespace AutoHub.Controllers
         }
 
         [HttpPost("{id:guid}/schedules")]
-        public async Task<ActionResult<IEnumerable<VehicleSchedule>>> AddVehicleSchedules([FromRoute] Guid userId, Guid id, [FromBody] IList<VehicleScheduleRequest> vehicleScheduleRequest)
+        public async Task<ActionResult<IEnumerable<VehicleSchedule>>> PostVehicleSchedules([FromRoute] Guid userId, Guid id, [FromBody] IList<VehicleScheduleRequest> vehicleScheduleRequest)
         {
             try
             {
                 IEnumerable<VehicleSchedule> vehicleSchedules = await _scheduledServiceTypeBizLogic.BatchAddVehicleSchedules(userId, id, vehicleScheduleRequest);
                 return Ok(vehicleSchedules);
+            }
+            catch (Exception ex)
+            {
+                var (statusCode, message) = ControllerExceptionHelper.HandleControllerException(ex);
+                return StatusCode(statusCode, message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ScheduledServiceType>> PostScheduledServiceType([FromRoute] Guid userId, [FromBody] ScheduledServiceTypeRequest request)
+        {
+            try
+            {
+                ScheduledServiceType scheduledServiceType = await _scheduledServiceTypeBizLogic.CreateScheduledServiceType(userId, request.Name);
+                return Ok(scheduledServiceType);
+            }
+            catch (Exception ex)
+            {
+                var (statusCode, message) = ControllerExceptionHelper.HandleControllerException(ex);
+                return StatusCode(statusCode, message);
+            }
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<ActionResult> DeleteScheduledServiceType([FromRoute] Guid userId, Guid id)
+        {
+            try
+            {
+                await _scheduledServiceTypeBizLogic.DeleteScheduledServiceType(userId, id);
+                return Ok();
             }
             catch (Exception ex)
             {
